4dfb6c06f3e53cae4a6e4d65caa87d800f2ea526
sasha-korn <korniichukaleksandra@gmail.com>
2024-07-28 23:07:52 +0100

    task 5 js

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+// console.log(getRandomHexColor());
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", changeBGC);
+
+function changeBGC() {
+  const color = getRandomHexColor();
+  const colorName = document.querySelector(".color");
+  colorName.innerHTML = color;
+  document.body.style.backgroundColor = color;
+}
