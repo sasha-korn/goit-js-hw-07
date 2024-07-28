93a5906978ad56f905e19e0e67edec2958bfcec4
sasha-korn <korniichukaleksandra@gmail.com>
2024-07-28 22:38:08 +0100

    solving of task 4 JS part

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const userForm = document.querySelector(".login-form");
 
+userForm.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const fieldEmail = document.querySelector('[name="email"]').value;
+  const fieldPsw = document.querySelector('[name="password"]').value;
+
+  if (fieldEmail == "" || fieldPsw == "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const user = {
+    email: fieldEmail,
+    password: fieldPsw,
+  };
+
+  console.log(user);
+  userForm.reset();
+}
